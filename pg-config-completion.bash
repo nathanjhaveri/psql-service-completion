e26d18f67c2d241ec5758fc19ac94de44f6ffd87
Nathan Jhaveri <nathanj@porch.com>
2018-07-06 16:50:23 -0700

    Fix completion of all entires

--- pg-config-completion.bash
+++ pg-config-completion.bash
@@ -2,18 +2,25 @@
 _psql_completions()
 {
     local word=${COMP_WORDS[COMP_CWORD]}
-    local line=${COMP_LINE}
+    #local line=${COMP_LINE}
     local complete_service=false
+    local prev="${COMP_WORDS[COMP_CWORD-1]}"
+    local prevprev="${COMP_WORDS[COMP_CWORD-2]}"
 
-    case "${line}" in
-    *--service*)
+    case "${prevprev}${prev}" in
+    service=)
         complete_service=true
+        ;;
+    psqlservice)
+        complete_service=true
+        word='' # Complete the line "psql service=" with all positibilites
         ;;
     esac
 
     if [ $complete_service = true ] ; then
-        COMPREPLY+=($(compgen -W "$(sed -n 's/\[\(.*\)\]/\1/p' .pg_service.conf)" -- "${word}"))
+        COMPREPLY+=($(compgen -W "$(sed -n 's/\[\(.*\)\]/\1/p' ~/.pg_service.conf)" -- "${word}"))
     fi
 }
 
 complete -F _psql_completions psql
+
